8f281d16022fb33be0d4fa2f6f0d76b53d6b5c8c
MaximusB93 <107109667+MaximusB93@users.noreply.github.com>
2026-06-26 21:37:24 +0300

    Add services barrel exports

---
+++ src/services/index.js
@@ -0,0 +1,7 @@
+export * from './ai-service.js';
+export * from './business-context-service.js';
+export * from './clients-service.js';
+export * from './integrations-service.js';
+export * from './optimization-service.js';
+export * from './performance-service.js';
+export * from './sync-service.js';
