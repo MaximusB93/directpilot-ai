5625654f6d832b67d56aa559c0fb4ff5a23dd23a
MaximusB93 <107109667+MaximusB93@users.noreply.github.com>
2026-06-30 13:39:21 +0300

    Export Wordstat page renderers

--- src/features/wordstat/index.js
+++ src/features/wordstat/index.js
@@ -2,3 +2,4 @@ export * from './wordstat-store.js';
 export * from './wordstat-service.js';
 export * from './wordstat-legacy-adapter.js';
 export * from './wordstat-controller.js';
+export * from './wordstat-page.js';
